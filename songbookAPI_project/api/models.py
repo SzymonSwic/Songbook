f3c142be40cc487f4a5ad360e46b5159b590f404
Szymon Swic <szymonswic@gmail.com>
2021-05-18 20:11:46 +0200

    first try with new views

--- songbookAPI_project/api/models.py
+++ songbookAPI_project/api/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
 
 
 class Song(models.Model):
@@ -6,7 +8,10 @@ class Song(models.Model):
     lyrics = models.TextField()
     author = models.CharField(max_length=60)
 
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField(default=timezone.now)
+
+    # def get_absolute_url(self):
+    #     return reverse('song_editor', args=[self.id])
 
     def __str__(self):
         return "{} - {}".format(self.author, self.title)
--- songbookAPI_project/api/urls.py
+++ songbookAPI_project/api/urls.py
@@ -8,5 +8,7 @@ router.register(r'songs', views.SongViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path('data/', include(router.urls)),
+    path('', views.SongListView.as_view(), name='song_list'),
+    path('<pk>/edit', views.SongUpdateView.as_view()),
 ]
